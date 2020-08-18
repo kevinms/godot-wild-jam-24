1ecd2481d0b5cec7672ad178fecca5c95df69c2b
Karl Brewer <drago3711@gmail.com>
2020-08-17 22:09:57 -0400

    Simple player walk animations

--- Player.gd
+++ Player.gd
@@ -8,6 +8,10 @@ const ACCEL = 2000
 const MAX_SPEED = 200
 const FRICTION = 1200
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 var velocity = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,8 +24,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACCEL * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
 	
 	move_and_slide(velocity)
