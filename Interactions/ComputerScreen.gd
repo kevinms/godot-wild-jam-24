d6dae743ed984d561eed3320418dc0415da3b5bb
Kevin Smith <ksmith.nop@gmail.com>
2020-08-19 19:14:58 -0400

    Computer mini game is half working now!

--- Interactions/ComputerScreen.gd
+++ Interactions/ComputerScreen.gd
@@ -25,7 +25,7 @@ func _ready():
 	for line in raw_lines:
 		while line.length() > max_line_length:
 			short_lines.append(line.left(max_line_length))
-			line = line.right(max_line_length)
+			line = line.right(max_line_length).strip_edges(true, false)
 		short_lines.append(line)
 	
 	# Loop over all lines
@@ -53,13 +53,39 @@ func _ready():
 	
 	current_line = 0
 	next_line = i
+	set_cursor()
 
 func key_press(letter):
+	print(letter)
+	
 	var line = source_lines[current_line]
 	var c = line[current_char]
 	
-	if c.text == letter:
-		c.color = Color(0,1,0)
+	if c.text != letter:
+		return
+	
+	c.color = Color(0,1,0)
+	
+	# Find next typeable character!
+	current_char += 1
+	while current_line < source_lines.size():
+		print("cl: ", current_line)
+		
+		# Look on the current line
+		while current_char < line.size():
+			print("cc: ", current_char)
+			if line[current_char].typeable:
+				print(line[current_char].text)
+				set_cursor()
+				return
+			current_char += 1
+		
+		current_char = 0
+		current_line += 1
+		del_line(line)
+		line = source_lines[current_line]
+	
+	#TODO: Reset everything if we reach this far.
 	
 	# As lines are typed:
 	#   Remove top line
@@ -76,15 +102,18 @@ func add_line(line: Array, y: float):
 func del_line(line: Array):
 	for c in line:
 		c.queue_free()
+	shift_up()
+
+func shift_up():
+	$TextPosition.translate(Vector2.UP * char_height)
+
+func set_cursor():
+	$TextPosition/ComputerCursor.position = Vector2(current_char * char_width, current_line * char_height)
 
 func load_text_file(path):
-	# Check if there is a saved file
 	var file = File.new()
-	if not file.file_exists(path):
-		print("No such file: ", path)
-		return ""
 	
-	# Open existing file
+	# Open file
 	var err = file.open(path, File.READ)
 	if err != OK:
 		printerr("Could not open file, error code ", err)
