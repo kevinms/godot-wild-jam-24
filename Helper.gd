f5aa9edd3286e001bf28ae87256024e34d6c378b
Kevin Smith <ksmith.nop@gmail.com>
2020-08-21 20:59:49 -0400

    Big update to the UI. It lost some of the features of the old one, so there is still lot's of work to do...

--- Helper.gd
+++ Helper.gd
@@ -1,5 +1,28 @@
 extends Node
 
+var player_name: String
+var baby_name: String
+var spouse_name: String
+
+var characters_types: int
+var typing_error_rate: int
+var game_quality: int
+var trash_disposed: int
+var baby_pooped: int
+var baby_diapers_changed: int
+var baby_bottles_used: int
+var pizza_eaten: int
+
+func reset():
+	characters_types = 0
+	typing_error_rate = 0
+	game_quality = 0
+	trash_disposed = 0
+	baby_pooped = 0
+	baby_diapers_changed = 0
+	baby_bottles_used = 0
+	pizza_eaten = 0
+
 func get_player():
 	for player in get_tree().get_nodes_in_group("player"):
 		return player
