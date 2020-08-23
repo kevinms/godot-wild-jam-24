ab130277a2e512c13756db4d89fff8976418e869
Karl Brewer <drago3711@gmail.com>
2020-08-23 03:02:26 -0400

    Upgraded the high scoreboard to display better

--- HighScores.gd
+++ HighScores.gd
@@ -6,13 +6,21 @@ func _ready():
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
+	if !json or !json.result:
+		return
 	print(json.result)
 	var rank = 1
 	for s in json.result["scores"]:
-		var node = Label.new()
-		node.text = "Rank: %s Game Quality: %s Player Name: %s Spouse Name: %s Baby Name: %s" % [ str(rank), s["game_quality"], s["player_name"], s["spouse_name"], s["baby_name"]]
-		$Control/VBoxContainer.add_child(node)
-		node.set_owner(get_tree().get_edited_scene_root())
+		$HBoxContainer/Rank.text += "\n" + str(rank)
+		$"HBoxContainer/Game Quality".text += "\n" + str(s["game_quality"])
+		$"HBoxContainer/Player Name".text += "\n" + s["player_name"]
+		$"HBoxContainer/Spouse Name".text += "\n" + s["spouse_name"]
+		$"HBoxContainer/Baby Name".text += "\n" + s["baby_name"]
+		
+		#var node = Label.new()
+		#node.text = "Rank: %s Game Quality: %s Player Name: %s Spouse Name: %s Baby Name: %s" % [ str(rank), s["game_quality"], s["player_name"], s["spouse_name"], s["baby_name"]]
+		#$Control/VBoxContainer.add_child(node)
+		#node.set_owner(get_tree().get_edited_scene_root())
 		rank += 1
 
 
