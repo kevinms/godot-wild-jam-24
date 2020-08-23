782dcc07836567083cf472b7a39798d672b32bef
Kevin Smith <ksmith.nop@gmail.com>
2020-08-22 21:31:11 -0400

    More tweaks to gameplay variables.

--- Helper.gd
+++ Helper.gd
@@ -35,11 +35,11 @@ const time_scale = 1.0
 
 const player_sleep_in_bed_min = (5*60) * time_scale # 5h sim time
 const player_sleepiness_per_min = 100.0 / (48.0 * 60.0) * time_scale # Every 48h sim time, sleepiness will be 100%
-const player_hungriness_per_min = 100.0 / (24.0 * 60.0) * time_scale # Every 24h sim time, hungriness will be 100%
+const player_hungriness_per_min = 100.0 / (36.0 * 60.0) * time_scale # Every 24h sim time, hungriness will be 100%
 const baby_sleepiness_per_min = 100.0 / (30.0 * 60.0) * time_scale # Every 30h sim time, sleepiness will be 100%
 const baby_hungriness_per_min = 100.0 / (24.0 * 60.0) * time_scale # Every 24h sim time, hungriness will be 100%
 const spouse_trash_hatred_per_item_per_min = 100.0 / (48.0 * 60.0) * time_scale # Every 24h sim time, happiness will be 0%
-const spouse_crying_hatred_per_min = 100.0 / (48.0 * 60.0) * time_scale # Every 24h sim time, happiness will be 0%
+const spouse_crying_hatred_per_min = 100.0 / (36.0 * 60.0) * time_scale # Every 24h sim time, happiness will be 0%
 const baby_crying_hatred_per_min = 100.0 / (24.0 * 60.0) * time_scale # Every 24h sim time, happiness will be 0%
 
 enum Threshold {
